73c553e6cba4de3de09f173776c48a079c42f351
szaboa <akos.szabo@jet-sol.hu>
2022-03-16 16:19:41 +0100

    fix style

--- components/layout/products.js
+++ components/layout/products.js
@@ -26,15 +26,15 @@ export default function Products() {
                 <button className={[styles.productsNavItem, activeTab == 'all' ? styles.productsNavItemUnderline : ''].join(' ')} onClick={() => setActiveTab('all')} tabIndex='0'>
                     ÖSSZES BOR
                 </button>
-                <button className={[styles.productsNavItem, { activeTab } == 'red' ? styles.productsNavItemUnderline : ''].join(' ')} onClick={() => setActiveTab('red')} tabIndex='1'>
+                <button className={[styles.productsNavItem, activeTab == 'red' ? styles.productsNavItemUnderline : ''].join(' ')} onClick={() => setActiveTab('red')} tabIndex='1'>
                     <div className={[styles.productsNavItemDot, styles.productsNavItemDotRed].join(' ')}></div>
                     VÖRÖS
                 </button>
-                <button className={[styles.productsNavItem, { activeTab } == 'white' ? styles.productsNavItemUnderline : ''].join(' ')} onClick={() => setActiveTab('white')} tabIndex='2'>
+                <button className={[styles.productsNavItem, activeTab == 'white' ? styles.productsNavItemUnderline : ''].join(' ')} onClick={() => setActiveTab('white')} tabIndex='2'>
                     <div className={[styles.productsNavItemDot, styles.productsNavItemDotWhite].join(' ')}></div>
                     FEHÉR
                 </button>
-                <button className={[styles.productsNavItem, { activeTab } == 'rose' ? styles.productsNavItemUnderline : ''].join(' ')} onClick={() => setActiveTab('rose')} tabIndex='3'>
+                <button className={[styles.productsNavItem, activeTab == 'rose' ? styles.productsNavItemUnderline : ''].join(' ')} onClick={() => setActiveTab('rose')} tabIndex='3'>
                     <div className={[styles.productsNavItemDot, styles.productsNavItemDotRose].join(' ')}></div>
                     ROZÉ
                 </button>
